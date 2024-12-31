870f97a478d73135b8207d89ab117883f47a4242
ghaidaareda <dodo.sa7afy@gmail.com>
2025-01-01 01:25:54 +0200

    data-sanitization

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const AppError = require('./utils/appError');
 const globlErrorHandlers = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 //body parser, read data from body into req.body
 
 app.use(express.json({ limit: '10kb' }));
+//data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+//data sanitization against XSS
+
+app.use(xss());
+
 //serve static files from folders not from server
 app.use(express.static(`${__dirname}/starter/public`));
 
