02dfcdeb2821a5a8c7563613d850922abb91c7c2
jailan-sh <jilan.elsherbiny@gmail.com>
2025-01-01 21:48:55 +0200

    embded user in the tours model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
+
 const tourSchema = new mongoose.Schema(
 	{
 		name: {
@@ -111,6 +113,12 @@ const tourSchema = new mongoose.Schema(
 				day: Number,
 			},
 		],
+		guides: [
+			{
+				type: mongoose.Schema.ObjectId,
+				ref: 'User',
+			},
+		],
 	},
 	{
 		toJSON: { virtuals: true },
@@ -128,10 +136,14 @@ tourSchema.pre('save', function (next) {
 	next();
 });
 
-// tourSchema.post('save', function (doc, next) {
-// 	console.log(doc);
-// 	next();
-// });
+//to return usersguide in the tourschemia
+tourSchema.pre('save', async function (next) {
+	const guidesPromises = this.guides.map(
+		async (id) => await User.findById(id)
+	);
+	this.guides = await Promise.all(guidesPromises);
+	next();
+});
 
 // query middleware for pre & post find query
 
