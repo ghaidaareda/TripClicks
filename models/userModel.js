f0dcc85083bd947f2eda9fa30773c3f74c0fd825
ghaidaareda <dodo.sa7afy@gmail.com>
2025-01-04 04:36:01 +0200

    data added

--- models/userModel.js
+++ models/userModel.js
@@ -6,11 +6,17 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: [true, 'name is required'],
+		required: [
+			true,
+			'name is required',
+		],
 	},
 	email: {
 		type: String,
-		required: [true, 'email is required'],
+		required: [
+			true,
+			'email is required',
+		],
 		unique: true,
 		Lowercase: true,
 		validate: [
@@ -21,18 +27,29 @@ const userSchema = new mongoose.Schema({
 	photo: { type: String },
 	role: {
 		type: String,
-		enum: ['user', 'guide', 'lead-guide', 'admin'],
+		enum: [
+			'user',
+			'guide',
+			'lead-guide',
+			'admin',
+		],
 		default: 'user',
 	},
 	password: {
 		type: String,
-		required: [true, 'password is required'],
+		required: [
+			true,
+			'password is required',
+		],
 		minlength: 8,
 		select: false,
 	},
 	passwordConfirmation: {
 		type: String,
-		required: [true, 'password confirmation is required'],
+		required: [
+			true,
+			'password confirmation is required',
+		],
 		validate: {
 			// only works on create & save not for update!!
 			validator: function (el) {
@@ -52,67 +69,100 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-userSchema.pre('save', async function (next) {
-	//run this function if password is modified
-	if (!this.isModified('password')) return next();
-	//has with cost of 12
-	this.password = await bcrypt.hash(this.password, 12);
-	//delete password confirm field
-	this.passwordConfirmation = undefined;
-	next();
-});
+userSchema.pre(
+	'save',
+	async function (next) {
+		//run this function if password is modified
+		if (!this.isModified('password'))
+			return next();
+		//has with cost of 12
+		this.password = await bcrypt.hash(
+			this.password,
+			12
+		);
+		//delete password confirm field
+		this.passwordConfirmation =
+			undefined;
+		next();
+	}
+);
 
 userSchema.pre('save', function (next) {
-	if (!this.isModified('password') || this.isNew)
+	if (
+		!this.isModified('password') ||
+		this.isNew
+	)
 		return next();
 
-	this.passwordChangedAt = Date.now() - 1000;
+	this.passwordChangedAt =
+		Date.now() - 1000;
 	next();
 });
 
-userSchema.pre(/^find/, function (next) {
-	//current quary
-	this.find({ active: { $ne: false } });
-	next();
-});
-
-userSchema.methods.correctPassword = async function (
-	candidatePassword,
-	userPassword
-) {
-	return bcrypt.compare(candidatePassword, userPassword);
-};
-
-userSchema.methods.changePasswordAfter = function (
-	JWTTimestamp
-) {
-	if (this.passwordChangedAt) {
-		const changedTimeStamp = parseInt(
-			this.passwordChangedAt.getTime() / 1000
-		);
-		//console.log(changedTimeStamp, JWTTimestamp);
-		return JWTTimestamp < changedTimeStamp;
+userSchema.pre(
+	/^find/,
+	function (next) {
+		//current quary
+		this.find({
+			active: { $ne: false },
+		});
+		next();
 	}
-	// user did not change the token after it was issued
-	return false;
-};
+);
+
+userSchema.methods.correctPassword =
+	async function (
+		candidatePassword,
+		userPassword
+	) {
+		return bcrypt.compare(
+			candidatePassword,
+			userPassword
+		);
+	};
+
+userSchema.methods.changePasswordAfter =
+	function (JWTTimestamp) {
+		if (this.passwordChangedAt) {
+			const changedTimeStamp = parseInt(
+				this.passwordChangedAt.getTime() /
+					1000
+			);
+			//console.log(changedTimeStamp, JWTTimestamp);
+			return (
+				JWTTimestamp < changedTimeStamp
+			);
+		}
+		// user did not change the token after it was issued
+		return false;
+	};
 
 // to reset password:
-userSchema.methods.createPasswordResetToken = function () {
-	const resetToken = crypto.randomBytes(32).toString('hex');
+userSchema.methods.createPasswordResetToken =
+	function () {
+		const resetToken = crypto
+			.randomBytes(32)
+			.toString('hex');
 
-	this.passwordResetToken = crypto
-		.createHash('sha256')
-		.update(resetToken)
-		.digest('hex');
+		this.passwordResetToken = crypto
+			.createHash('sha256')
+			.update(resetToken)
+			.digest('hex');
 
-	console.log({ resetToken }, this.passwordResetToken);
+		console.log(
+			{ resetToken },
+			this.passwordResetToken
+		);
 
-	this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+		this.passwordResetExpires =
+			Date.now() + 10 * 60 * 1000;
 
-	return resetToken;
-};
+		return resetToken;
+	};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model(
+	'User',
+	userSchema
+);
 
 module.exports = User;
--- starter/dev-data/data/import-dev-data.js
+++ starter/dev-data/data/import-dev-data.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const fs = require('fs');
+const User = require('../../../models/userModel');
 const Tour = require('../../../models/tourModel');
+const Review = require('../../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -20,13 +22,17 @@ mongoose
 	});
 
 //read json file
-const tours = JSON.parse(
-	fs.readFileSync(`${__dirname}/tours.json`, 'utf8')
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+const reviews = JSON.parse(
+	fs.readFileSync(`${__dirname}/reviews.json`, 'utf8')
 );
 //import to database
 const importData = async () => {
 	try {
 		await Tour.create(tours);
+		await User.create(users, { validateBeforeSave: false });
+		await Review.create(reviews);
 		console.log('data successfully loaded');
 	} catch (err) {
 		console.log(err);
@@ -38,6 +44,8 @@ const importData = async () => {
 const deleteData = async () => {
 	try {
 		await Tour.deleteMany();
+		await User.deleteMany();
+		await Review.deleteMany();
 		console.log('data successfully deleted');
 		process.exit();
 	} catch (err) {
