8081fb43f1a4e1ad992f5e085dd6202d34aeb52e
jailan-sh <jilan.elsherbiny@gmail.com>
2025-01-06 02:39:11 +0200

    edit some styling

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,12 +2,7 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
-	//get tour data from collection:
 	const tours = await Tour.find();
-
-	// build template
-
-	//render template
 
 	res.status(200).render('overview', {
 		title: 'All Tours',
