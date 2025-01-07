3be38573f3b3560bd19b02fad931180cb07396df
ghaidaareda <dodo.sa7afy@gmail.com>
2025-01-07 03:14:39 +0200

    sign-in-2

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const router = express.Router();
-//const authController = require('../controllers/authController');
+const authController = require('../controllers/authController');
+
+router.use(authController.isLogedIn);
 
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
--- starter/public/js/login.js
+++ starter/public/js/login.js
@@ -1,15 +1,21 @@
 /*eslint-disable */
 const login = async (email, password) => {
-	console.log(email, password);
 	try {
 		const res = await axios({
 			method: 'POST',
 			url: 'http://localhost:3000/api/v1/users/login',
 			data: { email, password },
 		});
+
+		if (res.data.status === 'success') {
+			alert('Login successfuly');
+			window.setTimeout(() => {
+				location.assign('/');
+			}, 1500);
+		}
 		console.log(res);
 	} catch (err) {
-		console.log(err.response.data);
+		alert(err.response.data.message);
 	}
 };
 
