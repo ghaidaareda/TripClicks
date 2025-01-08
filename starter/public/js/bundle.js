1ca48c388d46e6398acd58e5b8473222d7688a11
jailan-sh <jilan.elsherbiny@gmail.com>
2025-01-08 04:14:48 +0200

    built account edit photo error

--- starter/public/js/bundle.js
+++ starter/public/js/bundle.js
@@ -12872,7 +12872,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60123" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64027" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
